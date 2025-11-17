df3cb54716bdd807146432d0977e2b1ba3505780
Paradox-20002 <buttnoman778@gmail.com>
2025-11-17 17:15:38 +0500

    added favicon and updaetd the digital7 img from jfif to webp

--- app/services/digital-marketing/digital.jsx
+++ app/services/digital-marketing/digital.jsx
@@ -76,7 +76,7 @@ export default function Digital() {
       <section className="relative -mx-4 md:-mx-8 px-4 md:px-8 text-white overflow-hidden flex flex-col justify-center gap-8 min-h-screen">
         <div className="absolute inset-0 z-0">
           <Image
-            src="/images/digital7.jfif"
+            src="/images/digital7.webp"
             alt="Digital marketing background"
             fill
             priority
